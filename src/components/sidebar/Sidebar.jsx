31609a72fb0f80caf9364a7d7a5b22cf451a3ec4
Geet2601 <gt2601001gt@gmail.com>
2024-01-11 12:04:03 +0530

    update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import "./sidebar.scss"
 import Links from "./links/Links";
-import ToggleButton from "./toggleButton/toggleButton";
+import ToggleButton from "./toggleButton/ToggleButton";
 import { useState } from "react";
 import {motion} from "framer-motion";
 
